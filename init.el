88a7c3050c354a184c4410d6a9a27713c2721e32
NJAldwin <Nick@Aldwin.us>
2013-09-23 02:39:55 -0400

    Add web-mode

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 (add-to-list 'load-path "~/.emacs.d/modes/coffee-mode")
 (add-to-list 'load-path "~/.emacs.d/modes/markdown-mode")
 (add-to-list 'load-path "~/.emacs.d/modes/json-mode")
+(add-to-list 'load-path "~/.emacs.d/modes/web-mode")
 (add-to-list 'load-path "~/.emacs.d/color-theme/")
 (add-to-list 'load-path "~/.emacs.d/emacs-color-theme-solarized/")
 (add-to-list 'load-path "~/.emacs.d/deft/")
@@ -77,6 +78,24 @@
 (require 'protobuf-mode)
 (add-to-list 'auto-mode-alist
              (cons ".proto" 'protobuf-mode))
+
+;; Web Mode
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(defun web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'web-mode-hook)
 
 ;; Intelligent buffer renaming
 (require 'uniquify)
